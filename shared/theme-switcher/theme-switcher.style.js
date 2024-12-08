4b7ff881940df9c04791d26614390cbedbf60554
Teodora Chiosa <teodora.chiosa@gmail.com>
2024-12-08 21:28:45 +0200

    Update theme switcher button

--- shared/theme-switcher/theme-switcher.style.js
+++ shared/theme-switcher/theme-switcher.style.js
@@ -6,18 +6,47 @@ tCThemeSwitcherStyle.replaceSync(css`
         --size: 44px;
 
         position: relative;
+        display: flex;
+        justify-content: center;
+        align-items: center;
         width: var(--size);
         height: var(--size);
-        border: 0;
+        border: 3px solid color-mix(in hsl, var(--background), var(--body) 25%);
+        border-radius: 4px;
         background: transparent;
         color: var(--body);
 
         &::after {
-            display: block;
+            opacity: 0;
             content: '' attr(aria-label) '';
             position: absolute;
+            top: calc(100% + 8px);
+            left: 50%;
+            translate: -50%;
+            width: max-content;
+            background: color-mix(in hsl, var(--background), var(--body) 15%);
+            padding: 4px;
             color: var(--body);
-            background: var(--background);
+            border-radius: 10px;
+            transition: opacity 0.2s ease-in-out, box-shadow 0.2s ease-in-out,
+                display 0.2s ease-in-out allow-discrete;
+        }
+
+        &:hover,
+        &:focus-visible {
+            translate: -1px -1px;
+            box-shadow: 2px 2px 0 1px
+                color-mix(in hsl, var(--background), var(--body) 15%);
+
+            &::after {
+                opacity: 1;
+            }
+        }
+
+        &:active {
+            box-shadow: inset 2px 2px 0 1px
+                color-mix(in hsl, var(--background), var(--body) 25%);
+            background: color-mix(in hsl, var(--background), var(--body) 15%);
         }
     }
 `);
