f850f4cf0ff6d3a10b0e932c299e0fd6916c9ee8
Teodora Chiosa <teodora.chiosa@gmail.com>
2024-12-09 10:41:25 +0200

    Update sky.style.js to rem, Remove transition

--- layout/sky/sky.style.js
+++ layout/sky/sky.style.js
@@ -4,9 +4,8 @@ const tCSkyStyle = new CSSStyleSheet();
 tCSkyStyle.replaceSync(css`
     :host {
         display: block;
-        width: 400px;
+        width: 20rem;
         background: var(--sky-gradient);
-        transition: 2s ease-in-out;
     }
 `);
 export default tCSkyStyle;
