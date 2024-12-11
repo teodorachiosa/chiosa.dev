70f616f5274aa2e6ddf9509d3e31b1b9ca819935
Teodora Chiosa <teodora.chiosa@gmail.com>
2024-12-11 21:43:41 +0200

    Update sky.style.js

--- layout/sky/sky.style.js
+++ layout/sky/sky.style.js
@@ -3,9 +3,18 @@ const tCSkyStyle = new CSSStyleSheet();
 
 tCSkyStyle.replaceSync(css`
     :host {
+        --sky-gradient: linear-gradient(
+            -160deg in oklch,
+            var(--sky-gradient-color-a),
+            var(--sky-gradient-color-b)
+        );
+
         display: block;
         width: 22rem;
-        background: var(--sky-gradient);
+        background-image: var(--sky-gradient);
+        transition-duration: 0.3s;
+        transition-timing-function: ease;
+        transition-property: --sky-gradient-color-a, --sky-gradient-color-b;
     }
 `);
 export default tCSkyStyle;
