c211d6ec9c9353ff42241cd5ce7c84ee329ab8da
Teodora Chiosa <teodora.chiosa@gmail.com>
2024-12-11 21:43:00 +0200

    Update footer.template.js

--- layout/footer/footer.template.js
+++ layout/footer/footer.template.js
@@ -1,3 +1,32 @@
 const html = String.raw;
 
-export const tCFooterTemplate = html`<footer></footer>`;
+export const tCFooterTemplate = html`
+    <footer>
+        <ul>
+            <li>
+                <a
+                    aria-label="Linkedin"
+                    class="link linkedin"
+                    href="https://www.linkedin.com/in/teodora-chiosa"
+                ></a>
+            </li>
+            <li>
+                <a
+                    aria-label="Codepen"
+                    class="link codepen"
+                    href="https://codepen.io/teodorachiosa"
+                ></a>
+            </li>
+            <li>
+                <a
+                    aria-label="Cara"
+                    class="link cara"
+                    href="https://cara.app/teodorachiosa"
+                ></a>
+            </li>
+            <li>
+                <a class="link mail" href="mailto:placeholder@mail.com"></a>
+            </li>
+        </ul>
+    </footer>
+`;
