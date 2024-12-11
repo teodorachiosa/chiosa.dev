69ac774bcc18c888b0c5f763716a628178f9f650
Teodora Chiosa <teodora.chiosa@gmail.com>
2024-12-11 21:43:16 +0200

    Update main-container.style.js

--- layout/main-container/main-container.style.js
+++ layout/main-container/main-container.style.js
@@ -3,6 +3,9 @@ const tCMainContainerStyle = new CSSStyleSheet();
 
 tCMainContainerStyle.replaceSync(css`
     :host {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
         flex-grow: 1;
         margin: 90px 140px;
     }
