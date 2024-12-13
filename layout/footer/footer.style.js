55f2c0c2d7c0fd3ada02c849f6f2bfc5897a2eef
Teodora Chiosa <teodora.chiosa@gmail.com>
2024-12-13 19:39:24 +0200

    Update footer.style.js

--- layout/footer/footer.style.js
+++ layout/footer/footer.style.js
@@ -9,26 +9,30 @@ tCFooterStyle.replaceSync(css`
         list-style: none;
 
         li {
-            a {
+            a.link {
+                outline: none;
                 position: relative;
                 display: flex;
                 align-items: center;
                 justify-content: center;
-                padding: 12px;
                 color: var(--body);
-                transition: all 0.3s ease-in-out;
+                font-size: 1em;
                 border-radius: 50%;
+                transition: all 0.3s ease-in-out;
 
                 svg {
+                    box-sizing: content-box;
+                    padding: 10px;
                     width: 24px;
                     height: 24px;
+                    border-radius: 0.7rem;
                 }
 
-                &::after {
+                &::before {
                     content: '';
                     position: absolute;
                     z-index: -1;
-                    top: 2px;
+                    top: 10px;
                     right: 2px;
                     bottom: 2px;
                     left: 2px;
@@ -41,15 +45,42 @@ tCFooterStyle.replaceSync(css`
                     transition: all 0.3s ease-in-out;
                 }
 
+                &::after {
+                    opacity: 0;
+                    content: '' attr(aria-label) '';
+                    position: absolute;
+                    bottom: calc(100% + 0.3rem);
+                    left: 50%;
+                    translate: -50%;
+                    width: max-content;
+                    background-color: color-mix(
+                        in hsl,
+                        var(--background),
+                        var(--body) 15%
+                    );
+                    padding: 0.2rem 0.4rem;
+                    color: var(--body);
+                    border-radius: 0.5rem;
+                    transition: all 0.3s ease;
+                }
+
+                &:focus-visible {
+                    svg {
+                        outline: auto;
+                    }
+                }
+
                 &:hover,
                 &:focus-visible {
                     scale: 1.15;
-                    box-shadow: inset 0 1px 1px
-                        color-mix(in hsl, var(--background), var(--body) 15%);
 
-                    &::after {
+                    &::before {
                         opacity: 0.7;
                         filter: blur(5px);
+                    }
+
+                    &::after {
+                        opacity: 1;
                     }
                 }
             }
