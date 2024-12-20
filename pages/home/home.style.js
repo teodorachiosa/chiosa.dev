82d63a7ca5f422c5356bbad230e1be4c476f8e29
Teodora Chiosa <teodora.chiosa@gmail.com>
2024-12-20 22:45:38 +0200

    Update home.style.js

--- pages/home/home.style.js
+++ pages/home/home.style.js
@@ -1,5 +1,26 @@
 const css = String.raw;
 const tCHomeStyle = new CSSStyleSheet();
 
-tCHomeStyle.replaceSync(css``);
+tCHomeStyle.replaceSync(css`
+    p {
+        font-size: 2.5rem;
+        max-width: 28ch;
+        margin: 0;
+        font-weight: 200;
+
+        &:not(:last-child) {
+            margin-bottom: 1rem;
+        }
+
+        .my-name {
+            font-family: 'Philosopher', serif;
+            font-size: 1.07em;
+            font-weight: 700;
+        }
+
+        .my-passion {
+            font-weight: 400;
+        }
+    }
+`);
 export default tCHomeStyle;
