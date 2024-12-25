5cd86dd92ae1c0799793becbcdbeb8b508d128d8
Teodora Chiosa <teodora.chiosa@gmail.com>
2024-12-25 23:35:49 +0200

    Add main content animation

--- layout/main-container/main-container.template.js
+++ layout/main-container/main-container.template.js
@@ -1,7 +1,7 @@
 const html = String.raw;
 
 export const tCMainContainerTemplate = html`
-    <div class="main-container-max-width">
+    <div class="main-container-max-width fade-animation">
         <tc-header></tc-header>
         <main>
             <slot></slot>
